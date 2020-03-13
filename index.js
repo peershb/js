653dd61362dff24a8dd5a8487a76dca192c9d717
peershb <mpeer64@gmail.com>
2020-03-13 16:17:09 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,69 @@
+//  var number =12;
+//  var addOne = number-6;
+//   alert(number)
+
+//   var firstName = 'Peer'
+//   var lastName = 'Muhammad'
+//   alert (firstName + ' ' + lastName)
+// var totalDays = prompt('total days' , '')
+// var perDayIncomme = prompt('Per Day Income' , '')
+// var totalIncome = totalDays * perDayIncomme
+// alert('Your Total Income Is ' +totalIncome)
+// var countryName = prompt('Your Country Name Is' , '')
+// if(countryName === 'Pakistan'){
+//     alert('Jeay Bhutto')
+// }
+// else{
+//     alert('MQM Altaf Bhai Zindabad')
+// }
+
+// var number = prompt('Percentage')
+// if( number > 90)
+// {alert ('A+')
+// }
+// else if(number >=80 && number <=90)
+// {
+//     alert('A1')
+// }
+// else if(number>=70 && number<=80)
+// {
+//     alert('B')
+// }
+// else if(number>=60 && number<=70){
+//     alert('C')
+// }
+// else if(number>=50 && number<=60){
+//     alert('D')
+// }
+// else{
+//     alert('Tumharay Gaal pay Bousa dun')
+// }
+// var numberOfTanzeeem = parseInt(prompt('Tanzeem Ke Numbers'))
+// var numofMadarsa = parseInt(prompt('Madarse Ke Numbers'))
+// var totalNumbers =numberOfTanzeeem + numofMadarsa
+//     alert(totalNumbers)
+
+// var students =['Zeeshan' , 'Shahbaz' , 'Haseeb' , 'bahraam Shah']
+// students[4] = 'Amir'
+// students[5] = 'Gous'
+// students[6] = 'Hayat'
+// students.push('Hamza')
+// console.log(students)
+
+// students.pop()
+// console.log(students)
+
+// students.shift()
+// console.log(students)
+
+// students.unshift('Mansoor')
+// console.log(students)
+
+// students.splice(2 , 2 , 'Gulam' , 'Shafqat')
+// console.log(students)
+var name = prompt('Username')
+var students =['Zeeshan' , 'Shahbaz' , 'Haseeb' , 'bahraam Shah']   
+for (var i = 3; i >=0 ; i--){
+    if(name===students[i])
+alert([i])
+}    
